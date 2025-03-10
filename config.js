d197194647d46f020374547b496b5176dd78330d
Mori-Dev <162893646+araab-zack@users.noreply.github.com>
2025-03-10 17:07:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -13,8 +13,7 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["972546887176", '👑 Flash - Zack 👑', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
-["201224563219", '💫 Flash - Naruto 💫', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
+["201280244814", '👑 Flash - Z4cK & Mori 👑', true],//عدل الرقم والاسم وحط اسمك ورقمك
 ]
 
 
