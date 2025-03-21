6fa82851f5402316b6d916132448139dfeee30de
vfhghhfy <tt8211099@gmail.com>
2025-03-21 18:51:34 +0300

    Update تغير.js

--- plugins/تغير.js
+++ plugins/تغير.js
@@ -1,36 +1,60 @@
 import fs from 'fs'
-import jimp from 'jimp'
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const jimp = require('jimp')
 
 let handler = async (m, { conn, command, usedPrefix }) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg ? q.msg : q).mimetype || ''
-  if (/image/g.test(mime) && !/webp/g.test(mime)) {
-    try {
-      let media = await q.download()
-      let botNumber = conn.user.jid
-      let { img } = await pepe(media)
-      await conn.updateProfilePicture(botNumber, img) // استخدام الدالة المدمجة
-      m.reply('✅ تم تغيير صورة البروفايل بنجاح!')
-    } catch (e) {
-      console.error(e)
-      m.reply('❌ فشل التحديث، تأكد من الصورة وحاول لاحقًا.')
-    }
-  } else {
-    m.reply(`⚠️ أرسل صورة مع التسمية: ${usedPrefix + command}`)
+ let q = m.quoted ? m.quoted : m
+ let mime = (q.msg ? q.msg : q).mimetype ? q.mimetype : q.mediaType || ''
+ if (/image/g.test(mime) && !/webp/g.test(mime)) {
+  try {
+   let media = await q.download()
+   let botNumber = await conn.user.jid
+   let { img } = await pepe(media)
+   await conn.query({
+    tag: 'iq',
+    attrs: {
+     to: botNumber,
+     type:'set',
+     xmlns: 'w:profile:picture'
+    },
+    content: [
+     {
+      tag: 'picture',
+      attrs: { type: 'image/jpeg' },
+      content: img
+     }
+    ]
+   })
+   m.reply('*حطيتها يا حبي*')
+  } catch (e) {
+   console.log(e)
+   m.reply('حدث خطأ، حاول مرة أخرى لاحقًا')
   }
+ } else {
+  m.reply(`*أرسل الصورة مع التسمية التوضيحية ${usedPrefix + command}* \n*أو قم  بالاشارة  للصورة التي تريد وضعها كبروفايل البوت*`)
+ }
 }
 
-handler.help = ['setpp']
+handler.help = ['حطها_بروفايل']
 handler.tags = ['owner']
-handler.command = /^(تغير)$/i
-handler.owner = true
+handler.command = /^(حطها_بروفايل)$/i
+
+handler.rowner = true
 
 export default handler
 
 async function pepe(media) {
-  const image = await jimp.read(media)
-  const cropped = image.crop(0, 0, image.bitmap.width, image.bitmap.height)
-  return {
-    img: await cropped.scaleToFit(720, 720).getBufferAsync(jimp.MIME_JPEG)
-  }
+    const image = await jimp.read(media)
+    const min = image.getWidth()
+    const max = image.getHeight()
+    const cropped = image.crop(0, 0, min, max)
+    const buffer = await cropped
+      .scaleToFit(720, 720)
+      .getBufferAsync(jimp.MIME_JPEG)
+    return {
+     img: buffer.toString('base64'),
+     preview: await cropped.normalize().getBufferAsync(jimp.MIME_JPEG)
+    }
 }
